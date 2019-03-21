25cf3fb836c51b15c9602577ab9d04b8cfa240d6
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-21 19:26:54 +0000

    Deploy cilavery/better-weather to github.com/cilavery/better-weather.git:gh-pages

---
+++ precache-manifest.497a682897b4f18db62e0f4d47520314.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "1c08d35760ebe9d1c183",
+    "url": "/better-weather/static/css/main.61a1905d.chunk.css"
+  },
+  {
+    "revision": "1c08d35760ebe9d1c183",
+    "url": "/better-weather/static/js/main.fbf8ff7c.chunk.js"
+  },
+  {
+    "revision": "6df1680bea65ac5fc641",
+    "url": "/better-weather/static/js/runtime~main.21766090.js"
+  },
+  {
+    "revision": "d0bbbb742907fcf1cd05",
+    "url": "/better-weather/static/css/2.5435c558.chunk.css"
+  },
+  {
+    "revision": "d0bbbb742907fcf1cd05",
+    "url": "/better-weather/static/js/2.fec5a13f.chunk.js"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/better-weather/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/better-weather/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/better-weather/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/better-weather/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "ecaf8b481729b18f6a8494d9f691cdae",
+    "url": "/better-weather/static/media/weathericons-regular-webfont.ecaf8b48.svg"
+  },
+  {
+    "revision": "c8cdb7057b737231cbda2cc30269179e",
+    "url": "/better-weather/index.html"
+  }
+];
